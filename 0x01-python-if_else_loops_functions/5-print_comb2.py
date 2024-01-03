defa1a64c50eccbfd6c052a8aff4a17fa1bd1d30
Day-C <kongranalanyuy@gmail.com>
2024-01-03 02:38:49 -0800

    print all 2 combination numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -6,4 +6,4 @@ for number in range(100):
         if number < 99:
              print("{}".format(number), end=", ")
         else:
-            print("{}".format(number))
+            print(number)
