ca96f852a143e51b07cedda4fc30227def213a0a
Day-C <kongranalanyuy@gmail.com>
2024-01-03 03:09:38 -0800

    print all 2 combination numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for number in range(100):
-    if number < 10:
-        print("0{}".format(number), end=", ")
+    if number < 99:
+        print("{:02d}".format(number), end=", ")
     else:
-        if number < 99:
-             print("{}".format(number), end=", ")
-        else:
-            print(number)
+        print("{}".format(number))
