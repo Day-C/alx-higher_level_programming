48c195904944ad182891496e6b0fc9608607f5fc
Day-C <kongranalanyuy@gmail.com>
2024-01-04 02:29:36 -0800

    print sum of all command line arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import  sys
+
+    sum_ = 0
+    argv = sys.argv
+    if len(argv) == 1:
+        pass
+    else:
+        for i in range(1, len(argv)):
+           n = int(argv[i])
+           sum_ += n
+    print("{}".format(sum_))
+
