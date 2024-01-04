b925dc2263e322ccf373352dfc5d96570b02bf0b
Day-C <kongranalanyuy@gmail.com>
2024-01-04 01:43:25 -0800

    import multiple functions from file without *

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+calc = __import__('calculator_1')
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calc.div(a, b)))
