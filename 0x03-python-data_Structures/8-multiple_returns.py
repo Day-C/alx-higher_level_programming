1c8a6bca7e56ff6705234b3356cd45abef9bd71e
Day-C <kongranalanyuy@gmail.com>
2024-01-07 09:30:06 -0800

    returnint multiple results

---
+++ 0x03-python-data_Structures/8-multiple_returns.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == None:
+        return (0, None)
+    return (len(sentence), sentence[0])#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == None:
+        return (0, None)
+    return (len(sentence), sentence[0])#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == None:
+        return (0, None)
+    return (len(sentence), sentence[0])
