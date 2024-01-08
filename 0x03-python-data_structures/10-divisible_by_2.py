99bc9365bd92d57b1edbaf6480a203dd6a7ef14e
Day-C <kongranalanyuy@gmail.com>
2024-01-08 00:57:42 -0800

    testing

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    result = []
     for i in my_list:
-        if i == 0hnbnb
+        if i == 0:
+            result.append(True)
+            continue
+        if 2 % i == 0:
+            result.append(True)
+        result.append(False)
+    return (result)
