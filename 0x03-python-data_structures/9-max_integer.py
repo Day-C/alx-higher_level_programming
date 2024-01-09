bca81af6d7b5c995307ec8fd07014bf78b41298a
Day-C <kongranalanyuy@gmail.com>
2024-01-09 04:04:45 -0800

    get biggest number from a list of numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def  max_integer(my_list=[]):
+def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
-    max_num = 0
+    max_num = my_list[0]
     for i in my_list:
         if max_num > i:
             continue
