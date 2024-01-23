4ea6297dd54571bb4f07bd52dec613851020dd28
Day-C <kongranalanyuy@gmail.com>
2024-01-23 13:33:53 -0800

    accept variables:

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-class Square:
-    '''Create am empty class'''
+class Square
     pass
+'''empty class'''
---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Node():
+    '''class defines node of liked list'''
+    def __init__(self, data):
+        self.data = data
+
+    def data(self):
+
+    def data(self, value
