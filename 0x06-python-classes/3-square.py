abc19db5e793bbe0463d32e94e68ad2536f1fe84
Day-C <kongranalanyuy@gmail.com>
2024-01-24 14:41:26 -0800

    first method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,5 @@
-#!/usr/binn/python3
+#!/usr/bin/python3
+
 '''Create a class Square.'''
 
 
@@ -7,14 +8,12 @@ class Square:
     def __init__(self, size=0):
         '''Initialize variables.
         size'''
-        try:
-            size += 0
-        except:
+        if not isinstance(size, int):
             raise TypeError("size most be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self._size = size
+        self.__size = size
 
     def area(self):
         '''Method returns the square area.'''
-        return (self._size * self._size)
+        return (self.__size * self.__size)
