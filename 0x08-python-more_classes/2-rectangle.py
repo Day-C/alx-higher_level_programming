f982bebef7c7654fe34264d37ffc16006aa085ab
Dayc <kongranalanyuy@gmail.com>
2024-01-31 03:03:43 -0800

    fixe with space after return

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -70,7 +70,7 @@ class Rectangle:
 
         if self.__height == 0 or self.__width == 0:
             return (0)
-        return(self.__height * self.__width)
+        return (self.__height * self.__width)
 
     def perimeter(self):
         '''Perimeter of rectangle.'''
