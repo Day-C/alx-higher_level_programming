4a6a1098ff3bfc726051e7f8b0deec9bd2613c4a
Dayc <kongranalanyuy@gmail.com>
2024-02-05 14:36:39 -0800

    uninitialiezed method variables

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry:
     def area(self):
         '''Method returns exception.'''
 
-        raise Exception("area{} is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         '''Method validates value.'''
