a25e0baf82d60ca78b20c237b59a6ea887c2ecd3
Dayc <kongranalanyuy@gmail.com>
2024-02-06 11:36:12 -0800

    json representation of object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Define function to_json_string().'''
+import json
+
+
+def to_json_string(my_obj):
+    '''Convert my_obj to a json representation.'''
+
+    return (json.dumps(my_obj))
