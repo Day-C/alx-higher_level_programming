25828342a066435ddb24b1fa85943f9fdc2b91a8
Dayc <kongranalanyuy@gmail.com>
2024-02-06 07:43:07 -0800

    read content from file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Define function read_file().'''
 
-def read_file(filename=""):
+def read_file(filename="", encoding = "utf-8"):
     '''Read contents of file.'''
 
     with open(filename) as f:
