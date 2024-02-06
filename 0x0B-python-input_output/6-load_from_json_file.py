cc559aae2558b1bc77cef13e70658401ee15b578
Dayc <kongranalanyuy@gmail.com>
2024-02-06 14:49:33 -0800

    create an object from a json files contents

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ def load_from_json_file(filename):
     '''Function creates objects from content of json file.'''
 
     with open(filename, encoding="utf-8") as f:
-            data = f.read()
-            return (json.loads(data))
+        data = f.read()
+        return (json.loads(data))
