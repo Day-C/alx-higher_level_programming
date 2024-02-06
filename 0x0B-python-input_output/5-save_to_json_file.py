c9541f7511e9b0983d2dcea724e8855c8a931f64
Dayc <kongranalanyuy@gmail.com>
2024-02-06 12:56:22 -0800

    saving text to json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Define function save_to_json_file().'''
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''Convert object to json_string ans save in file.'''
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write = json.dumps(my_obj)
+        print(json.dumps(my_obj))
