9e3d538f312ecefaf864e4efbfa3c1e4f5e02abf
Dayc <kongranalanyuy@gmail.com>
2024-02-06 16:10:08 -0800

    display dictionary representation of class to json_string

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,8 +2,7 @@
 '''Define a function class_to_json().'''
 
 
-
 def class_to_json(obj):
     '''Convert class to json string.'''
 
-    return (json.dumps((obj.__dict__)))
+    return ((obj.__dict__))
