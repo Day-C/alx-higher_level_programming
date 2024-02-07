f210dbf297585fa6cbd697530fb4962a9bdffb99
Dayc <kongranalanyuy@gmail.com>
2024-02-06 16:24:11 -0800

    retrive dictionary representation of a class instace

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,11 +8,13 @@ class Student:
     def __init__(self, first_name, last_name, age):
         '''Initialize public attributes.'''
 
+
         self.first_name = first_name;
         self.last_name = last_name;
         self.age = age;
 
     def to_json(self):
         '''Retrive dictionary representation of a Student instance.'''
+
 
         return (self.__dict__)
