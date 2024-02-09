ab78ba037dbc1fd403cf5a29aa5244720376eb2b
Dayc <kongranalanyuy@gmail.com>
2024-02-09 11:21:13 -0800

    print 2-d array with '#'

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -14,10 +14,12 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     @property
     def size(self):
         '''Module retuns size.'''
         return (self.__size)
+
     @size.setter
     def size(self, value):
         '''Method set new valuew of size.'''
@@ -26,19 +28,18 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         '''Method returns the suare of a number.'''
         return (self.__size *self.__size)
+
     def my_print(self):
         '''Method prints a square with '#' characters.'''
         j = 0
         if self.__size != 0:
-            for i in range((self.__size * self.__size) - 1):
-                if j == self.__size:
-                    j = 0
-                    print("")
-                else:
+            for i in range(self.__size):
+                for j in range(self.__size):
                     print("#", end="")
-                    j += 1
+                print("")
         else:
             print("")
