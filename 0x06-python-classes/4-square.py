8b1f46aa526209e399f6ca6a03dc0b89b3e8b645
Dayc <kongranalanyuy@gmail.com>
2024-02-09 11:01:51 -0800

    pystyle check

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,7 +6,6 @@
 class Square:
     '''Structure class.'''
 
-
     def __init__(self, size=0):
         '''Initialize variables.
         Arg: size'''
@@ -15,10 +14,12 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    
     @property
     def size(self):
         '''Method gets the current size of square.'''
         return (self.__size)
+    
     @size.setter
     def size(self, value):
         '''Method sets the value of a square.'''
@@ -27,6 +28,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+    
     def area(self):
         '''Method returs the area of that square.'''
         return (self.__size * self.__size)
