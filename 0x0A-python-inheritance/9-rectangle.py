f0b127a259490572f62f9d6e09de145875ead1ad
Dayc <kongranalanyuy@gmail.com>
2024-02-10 14:20:59 -0800

    indentation matters

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -14,12 +14,8 @@ class Rectangle(BaseGeometry):
         self.__height = height
         self.__width = width
 
-
     def area(self):
         return (self.__width * self.__height)
-
-    def print(self):
-        print("[Rectangle] {}/{}".format(self.__width, self.__height))
 
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
