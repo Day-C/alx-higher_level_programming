398e2e987df0dd00a03783c946861b35257d3453
Dayc <kongranalanyuy@gmail.com>
2024-02-10 07:14:02 -0800

    use type(obj) == class to check if object is  an instance of specific class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Define class is_same_class.'''
+
+def is_same_class(obj, a_class):
+    '''Checks if object is an instance of specified class.'''
+
+    if type(obj) == a_class:
+        return (True)
+    else:
+        (False)
