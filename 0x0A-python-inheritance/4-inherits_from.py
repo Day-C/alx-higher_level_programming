d682a5ba6c3e46d9634144000a32a43c3a5b1524
Dayc <kongranalanyuy@gmail.com>
2024-02-10 07:58:07 -0800

    used if type(obj) is not class_name to check if an object is an instance of a class that inherited(directly or indirectly) from the specified class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,9 +3,9 @@
 
 
 def inherits_from(obj, a_class):
-    '''Check if object is an instance of a class that inherits from specific class.'''
+    '''Check if object is an instance of inherited class.'''
 
-    if type(obj) is not a_class:
+    if type(obj) is not a_class and issubclass(type(obj), a_class):
         return (True)
     else:
         return (False)
