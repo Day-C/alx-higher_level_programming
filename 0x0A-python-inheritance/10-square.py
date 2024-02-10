89ec11fc707433d55cc2593bcc941261c7a1c7be
Dayc <kongranalanyuy@gmail.com>
2024-02-10 14:40:34 -0800

    double inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Define class Square.'''
+Rectangle =  __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Inherit from Rectangle.'''
+
+    def __init__(self, size):
+        '''Initialize attributes.'''
+
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''Area of square.'''
+
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        '''print string prepresentation for class.'''
+
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
